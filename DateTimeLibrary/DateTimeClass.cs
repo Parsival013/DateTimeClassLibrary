43004dbc23aff844650c7fd02374f89d4755aee2
Alex <alex69hu196@gmail.com>
2022-10-24 21:21:53 +0300

    First Commit

---
+++ DateTimeLibrary/DateTimeClass.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeLibrary
+{
+    public class DateTimeClass
+    {
+        /// <summary>
+        /// Метод определяющий дату предыдущего дня
+        /// </summary>
+        /// <param name="DateTimes">Вводимая дата</param>
+        /// <returns>Возвращает введеную дату - 1 день</returns>
+        public static DateTime PrevDate(DateTime DateTimes)
+        {
+            if (DateTimes == DateTime.MinValue)
+            {
+                throw new Exception("Введена неверная дата");
+            }
+            return DateTimes.Subtract(TimeSpan.FromDays(1));
+        }
+        /// <summary>
+        /// Определяет дату следующего дня
+        /// </summary>
+        /// <param name="DateTimes">Вводимая дата</param>
+        /// <returns>Возвращает введеную дату + 1 день</returns>
+        public static DateTime NextDate(DateTime DateTimes)
+        {
+            if (DateTimes == DateTime.MinValue)
+            {
+                throw new Exception("Введена неверная дата");
+            }
+            return DateTimes.Add(TimeSpan.FromDays(1));
+        }
+        /// <summary>
+        /// Определяет количество дней до конца месяца
+        /// </summary>
+        /// <param name="DateTimes">Вводимая дата</param>
+        /// <returns>Возвращает количество дней в определенном месяце - сегодняшний день</returns>
+        public static int DaysUntilEndMonth(DateTime DateTimes)
+        {
+            if (DateTimes == DateTime.MinValue)
+            {
+                throw new Exception("Введена неверная дата");
+            }
+            return DateTime.DaysInMonth(DateTimes.Year, DateTimes.Month) - DateTimes.Day;
+        }
+        /// <summary>
+        /// Определяет високосный год сейчас или нет
+        /// </summary>
+        /// <param name="DateTimes">Вводимая дата</param>
+        /// <returns>Определяет високосный год сейчас или нет по вводимой дате</returns>
+        public bool LeapYearCheck(DateTime DateTimes)
+        {
+            if (DateTimes == DateTime.MinValue)
+            {
+                throw new Exception("Введена неверная дата");
+            }
+            return DateTime.IsLeapYear(DateTimes.Year);
+        }
+        /// <summary>
+        /// определить дату i-того по счету дня относительно установленной даты
+        /// </summary>
+        /// <param name="DateTimes">Вводимая дата</param>
+        /// <returns>возвращает дату i-того по счету дня относительно установленной даты</returns>
+        public static DateTime FindDate(DateTime DateTimes, int i)
+        {
+            if (DateTimes == DateTime.MinValue)
+            {
+                throw new Exception("Введена неверная дата");
+            }
+            return DateTimes.AddDays(i);
+        }
+        /// <summary>
+        /// Определяет установленная дата является ли последним днем месяца или нет
+        /// </summary>
+        /// <param name="DateTimes">Вводимая дата</param>
+        /// <returns>возвращает значение true, если установленная дата не является последним днем месяца, иначе false</returns>
+        public bool NextDayOfMonth(DateTime DateTimes)
+        {
+            if (DateTimes == DateTime.MinValue)
+            {
+                throw new Exception("Введена неверная дата");
+            }
+            if (DateTimes.Day == DateTime.DaysInMonth(DateTimes.Year, DateTimes.Month))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        /// <summary>
+        /// Определяет установленная дата является ли началом года или нет
+        /// </summary>
+        /// <param name="DateTimes">Вводимая дата</param>
+        /// <returns>выдает значение true, если установленная дата является началом года, иначе false</returns>
+        public bool FirstDayOdYear(DateTime DateTimes)
+        {
+            if (DateTimes == DateTime.MinValue)
+            {
+                throw new Exception("Введена неверная дата");
+            }
+            if (DateTimes.Day == 1 && DateTimes.Month == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ DateTimeLibraryTests/DateTimeClassTests.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DateTimeLibrary;
+
+namespace DateTimeLibraryTests
+{
+    [TestClass]
+    public class DateTimeClassTests
+    {
+        /// <summary>
+        /// метод, определяющий предыдущий день
+        /// </summary>
+        [TestMethod]
+        public void PrevDate_DetermineThePreviousDay_ReturnedPreviousDay()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2022, 10, 8);
+            DateTime excepted = new DateTime(2022,10,7);
+            //Act
+
+            DateTime actual = DateTimeClass.PrevDate(DateTimes);
+            //Assert
+            Assert.AreEqual(actual, excepted);
+        }
+        /// <summary>
+        /// метод, определяющий следующий день
+        /// </summary>
+        [TestMethod]
+        public void NextDate_DetermineTheNextDay_ReturnedNextDay()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2022, 10, 8);
+            DateTime excepted = new DateTime(2022, 10, 9);
+            //Act
+            DateTime actual = DateTimeClass.NextDate(DateTimes);
+            //Assert
+            Assert.AreEqual(actual, excepted);
+        }
+        /// <summary>
+        /// метод, устанавливающий, является ли введенный день последний в месяце или нет
+        /// </summary>
+        [TestMethod]
+        public void NextDayOfMonth_DataNotLastDayMonth_ReturnedTrue()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2022, 10, 8);
+
+            //Act
+            DateTimeClass c = new DateTimeClass();
+            bool actual = c.NextDayOfMonth(DateTimes);
+            //Assert
+            Assert.IsTrue(actual);
+
+        }
+        /// <summary>
+        /// метод, устанавливающий, является ли введенный день последний в месяце или нет
+        /// </summary>
+        [TestMethod]
+        public void NextDayOfMonth_DataLastDayMonth_ReturnedFalse()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2022, 10, 31);
+
+            //Act
+            DateTimeClass c = new DateTimeClass();
+            bool actual = c.NextDayOfMonth(DateTimes);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+        /// <summary>
+        /// метод, устанавливающий, является ли введенный день первым днем в году или нет
+        /// </summary>
+        [TestMethod]
+        public void FirstDayOdYear_DataItFistDayYear_ReturnedTrue()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2022, 1, 1);
+
+            //Act
+            DateTimeClass c = new DateTimeClass();
+            bool actual = c.FirstDayOdYear(DateTimes);
+            //Assert
+            Assert.IsTrue(actual);
+
+        }
+        /// <summary>
+        /// метод, устанавливающий, является ли введенный день первым днем в году или нет
+        /// </summary>
+        [TestMethod]
+        public void FirstDayOdYear_DataItNotFistDayYear_ReturnedFalse()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2022, 1, 2);
+
+            //Act
+            DateTimeClass c = new DateTimeClass();
+            bool actual = c.FirstDayOdYear(DateTimes);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+        /// <summary>
+        /// метод, который позволяет определить сколько дней осталось до конца месяца
+        /// </summary>
+        [TestMethod]
+        public void DaysUntilEndMonth_DeterminingTheDaysUntilTheEndOfTheMonth_ReturnedNumberOfDays()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2022, 1, 1);
+
+            int excepted = 30;
+            //Act
+            int actual = DateTimeClass.DaysUntilEndMonth(DateTimes);
+            //Assert
+            Assert.AreEqual(actual, excepted);
+
+        }
+        /// <summary>
+        /// метод, который позволяет определить что год не високосный
+        /// </summary>
+        [TestMethod]
+        public void LeapYearCheck_ItsNotLeapYear_ReturnedFalse()
+        {
+            //Arrange
+            DateTime DateTimes = new DateTime(2019);
+
+            //Act
+            DateTimeClass c = new DateTimeClass();
+            bool actual = c.LeapYearCheck(DateTimes);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+        
+
+    }
+}
